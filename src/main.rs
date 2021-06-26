f048d2e3e5019374a78cf7c49d789bbc84eabc9f
Miguel García Soler <miguelgarciasoler0@gmail.com>
2021-06-27 00:48:40 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+// This is tic-tac-toe
+
+fn print_board() {
+    println!(" | | ");
+    println!("-----");
+    println!(" | | ");
+    println!("-----");
+    println!(" | | ");
+}
+
+fn main() {
+    println!("Hello, world!");
+}
